2addaceaea51944c40c55a24b23ecfa298caef64
kusano koki <kusano@ai.cs.kobe-u.ac.jp>
2020-03-10 11:55:33 +0900

    もっと楽に

--- annotation.py
+++ annotation.py
@@ -1,11 +1,15 @@
+import pdb
 import pandas as pd
 import numpy as np
 import os
 from glob import glob
 from PIL import Image
 import matplotlib.pyplot as plt
+import cv2
 
-import pdb
+'''
+
+'''
 
 
 def main():
@@ -14,8 +18,11 @@ def main():
         os.environ['HOME'])
 
     # ここも各自でよしなに
-    csv_path = "{}/Desktop/N2C2_DataSet/emo_received.csv".format(
+    csv_path = "{}/Desktop/N2C2_DataSet/f_test.csv".format(
         os.environ['HOME'])
+
+    face_cascade_path = '/usr/local/opt/opencv/share/OpenCV/haarcascades/haarcascade_frontalface_alt.xml'
+    face_cascade = cv2.CascadeClassifier(face_cascade_path)
 
     data_path_list = np.array(sorted(glob(data_dir_path)))
     if os.path.exists(csv_path):
@@ -25,6 +32,17 @@ def main():
         arr = np.vstack([data_path_list, labels]).T
         df = pd.DataFrame(arr, columns=["x:image", "y:label"])
         df.to_csv(csv_path, index=False)
+
+        for index, row in df.iterrows():
+            img = cv2.imread(row['x:image'])
+            im_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+            faces = face_cascade.detectMultiScale(im_gray)
+            if len(faces) > 0:
+                pass
+            else:
+                df.at[index, 'y:label'] = 0
+            df.to_csv(csv_path, index=False)
+    pdb.set_trace()
 
     # labelがついてない状態ではNoneラベルがついています
     for index, row in df.iterrows():
